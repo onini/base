b401dc06fab5b5df604a627f784131c406c5311f
太年轻 <tthd@163.com>
2017-12-26 08:59:00 +0800

    init

---
+++ src/Providers/OniniServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace One\Base\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OniniServiceProvider extends ServiceProvider
+{
+	/**
+     * Register the application services.
+     *
+     * @return void
+     */
+	 public function register()
+	 {
+
+	 }
+
+	 /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+	 public function boot()
+	 {
+
+	 }
+}
